7fc869d072b2ab699a0f9ae039ec43f8231c2d48
Dakota Sankey <dakotasankey@gmail.com>
2021-04-09 14:31:35 -0700

    better logging

--- src/porygon/logger.ts
+++ src/porygon/logger.ts
@@ -1,20 +1,27 @@
-import { isDev } from 'support/dev';
 import { createLogger, transports, format } from 'winston';
+
+const formatInline = format.printf((info) => {
+  return `${info.timestamp} ${info.level}: ${info.message}`;
+});
 
 export const logger = createLogger({
   level: 'info',
-  format: format.json(),
   transports: [
-    new transports.File({ filename: 'error.log', level: 'error' }),
-    new transports.File({ filename: 'combined.log' }),
-  ],
-});
-
-if (isDev) {
-  logger.add(
+    new transports.File({
+      filename: 'error.log',
+      level: 'error',
+      format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        formatInline,
+      ),
+    }),
     new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
+      format: format.combine(
+        format.timestamp({ format: 'HH:mm:ss' }),
+        format.colorize(),
+        formatInline,
+      ),
       level: 'debug',
     }),
-  );
-}
+  ],
+});
