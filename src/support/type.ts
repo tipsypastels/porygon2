5ea643ca20e9f0660cc88cb67d39206d9c991a36
Dakota Sankey <dakotasankey@gmail.com>
2021-04-18 01:30:26 -0700

    magic is real

--- src/support/type.ts
+++ src/support/type.ts
@@ -1,6 +1,28 @@
-/**
- * Removes `never` values from an object type.
- */
-export type RemoveNever<T extends Record<string, unknown>> = {
-  [K in keyof T as T[K] extends never ? never : K]: T[K];
+type X = {
+  a: number;
+  b: {
+    c: number;
+    d: number;
+    e: {
+      f: bigint;
+    };
+  };
 };
+
+type AnyObject = Record<string, unknown>;
+
+type FlattenPaths<T> = T extends AnyObject ? FlattenObject<T> : T;
+
+type FlattenObject<T extends AnyObject> = {
+  [K in keyof T as FlattenKey<T, K>]: T[K] extends AnyObject
+    ? FlattenPaths<T[K][keyof T[K]]>
+    : FlattenPaths<T[K]>;
+};
+
+type FlattenKey<T, K extends keyof T> = T[K] extends AnyObject
+  ? `${string & K}.${string & keyof T[K]}`
+  : K;
+
+type x = FlattenPaths<X>;
+
+type a = x['b.e'];
