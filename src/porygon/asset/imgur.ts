19181fc4d74bb560fec1ce1a27384cbd33b19358
Dakota Sankey <dakotasankey@gmail.com>
2021-06-01 14:54:00 -0700

    add err logging for imgur

--- src/porygon/asset/imgur.ts
+++ src/porygon/asset/imgur.ts
@@ -1,15 +1,21 @@
 import { IMGUR_CLIENT } from 'secrets.json';
 import { ImgurClient } from 'imgur';
 import type { Asset } from './index';
+import { logger } from 'porygon/logger';
 
 const imgur = new ImgurClient({ clientId: IMGUR_CLIENT });
 
-export async function upload(asset: Asset) {
-  const res = await imgur.upload(asset.path);
-
-  if (Array.isArray(res)) {
-    return res[0].data.link;
-  }
-
-  return res.data.link;
+export function upload(asset: Asset) {
+  return imgur
+    .upload(asset.path)
+    .then((res) => {
+      if (Array.isArray(res)) {
+        return res[0].data.link;
+      }
+      return res.data.link;
+    })
+    .catch((e) => {
+      logger.error('Imgur upload failed.');
+      throw e;
+    });
 }
